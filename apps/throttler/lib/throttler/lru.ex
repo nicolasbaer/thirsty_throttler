e08c5a0fa910f2bb2f258e49cb891b69707ea754
Robert Erdin <robert.erdin@gmail.com>
2016-12-18 04:27:54 +0100

    lru working without housekeeping

--- apps/throttler/lib/throttler/lru.ex
+++ apps/throttler/lib/throttler/lru.ex
@@ -2,18 +2,16 @@ defmodule Throttler.LRU do
 
     def throttle(session_id) do
 
-        IO.puts LruCache.get(:sessions, session_id)
+        IO.inspect :ets.info(:sessions) 
 
         case LruCache.get(:sessions, session_id) do
-            nil -> if :ets.info(:sessions, :size) > 3 do 
-                    true 
-                else  
-                    LruCache.put(:sessions, session_id, "skjfsa")
-                    false
-                end
+            nil ->  if :ets.info(:sessions, :size) >= 3 do 
+                        true 
+                    else  
+                        LruCache.put(:sessions, session_id, "skjfsa")
+                        false
+                    end
             _ -> false
         end
     end
-
-
 end
