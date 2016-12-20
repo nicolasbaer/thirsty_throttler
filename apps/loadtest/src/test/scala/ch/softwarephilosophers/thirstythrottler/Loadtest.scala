8a9d9c4036852dfbe2b604055af19936c5752b43
Nicolas Bär <nicolas.baer@gmail.com>
2016-12-20 14:27:37 +0100

    loadtests with site awareness

--- apps/loadtest/src/test/scala/ch/softwarephilosophers/thirstythrottler/Loadtest.scala
+++ apps/loadtest/src/test/scala/ch/softwarephilosophers/thirstythrottler/Loadtest.scala
@@ -15,8 +15,39 @@ class StressTest extends Simulation {
     .baseURL("http://localhost:8080")
     
   setUp(
-    scenario("most-important").exec(http("cart").get("/cart")).inject(constantUsersPerSec(20) during(5 minutes)).protocols(httpConf),
-    scenario("less-important").exec(http("home").get("/home")).inject(constantUsersPerSec(20) during(5 minutes)).protocols(httpConf),
-    scenario("senseless").exec(http("foo").get("/foo")).inject(constantUsersPerSec(20) during(5 minutes)).protocols(httpConf)
+    scenario("most-important")
+      .exec(http("cart").get("/cart").check(header("sessionid").saveAs("sessionid")))
+      .exec(http("cart").get("/cart").header("sessionid", "${sessionid}"))
+      .exec(http("cart").get("/cart").header("sessionid", "${sessionid}"))
+      .exec(http("cart").get("/cart").header("sessionid", "${sessionid}"))
+      .exec(http("cart").get("/cart").header("sessionid", "${sessionid}"))
+      .inject(constantUsersPerSec(5) during(5 minutes)).protocols(httpConf),
+    scenario("less-important")
+      .exec(http("home").get("/home").check(header("sessionid").saveAs("sessionid")))
+      .exec(http("home").get("/home").header("sessionid", "${sessionid}"))
+      .exec(http("home").get("/home").header("sessionid", "${sessionid}"))
+      .inject(constantUsersPerSec(5) during(5 minutes)).protocols(httpConf),
+    scenario("senseless")
+      .exec(http("foo").get("/foo"))
+      .inject(constantUsersPerSec(5) during(5 minutes)).protocols(httpConf)
+  )
+}
+
+
+class StressTestRemote extends Simulation {
+
+  val httpConf = http
+    .baseURL("http://192.168.2.244:8080")
+    
+  setUp(
+    scenario("most-important")
+      .exec(http("cart").get("/cart"))
+      .inject(constantUsersPerSec(100) during(1 minutes)).protocols(httpConf),
+    scenario("less-important")
+      .exec(http("home").get("/home"))
+      .inject(constantUsersPerSec(100) during(1 minutes)).protocols(httpConf),
+    scenario("senseless")
+      .exec(http("foo").get("/foo"))
+      .inject(constantUsersPerSec(100) during(1 minutes)).protocols(httpConf)
   )
 }
